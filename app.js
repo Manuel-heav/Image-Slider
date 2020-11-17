2fc40f6e0ad48b47beece31ff1078c6c0290ccd3
Manuel-heav <Manuelheav32@gmail.com>
2020-11-18 02:39:57 +0700

    Project successfully done!

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ const images = ['images/image1.jpg','images/image2.jpg','images/image3.jpg','ima
 let i = 1;
 // Functions
 function backwards(){
- document.querySelector("#singleImg").src=`images/image${i}.jpg`;
  i--;
- console.log(i)
+ if(i <= 0){
+ 	i = 4;
+ }
+ // console.log(i)
+ document.querySelector("#singleImg").src=`images/image${i}.jpg`;
+ 
+
 }
 
 function forwards(){
- document.querySelector("#singleImg").src=`images/image${i}.jpg`;
  i++;
- console.log(i)
+  if(i >= 5){
+ 	i = 1;
+ }
+ document.querySelector("#singleImg").src=`images/image${i}.jpg`;
+
+ // console.log(i)
 }
 // Event Listeners
 leftBtn.addEventListener('click', backwards);
